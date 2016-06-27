cdc3253ce2f51d4a13b924cdce84dd128420be38
Kiswanto D <kiswanto.d21@gmail.com>
2016-06-27 12:37:47 +0700

    repair selectize

--- add.php
+++ add.php
@@ -15,7 +15,8 @@ include("koneksi.php");
 	<!-- Bootstrap -->
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/bootstrap-datepicker.css" rel="stylesheet">
-	
+	<link href="selectize/css/selectize.bootstrap3.css" rel="stylesheet">
+
 	<style>
 		.content {
 			margin-top: 80px;
@@ -48,10 +49,12 @@ include("koneksi.php");
 			</div><!--/.nav-collapse -->
 		</div>
 	</nav>
+
 	<div class="container">
 		<div class="content">
+
 			<h2>Data Badan Usaha &raquo; Tambah Data</h2>
-			<hr />
+			<hr>
 			
 			<?php
 			if(isset($_POST['add'])){
@@ -72,15 +75,18 @@ include("koneksi.php");
 				$status			 = $_POST['status'];
 				$keterangan	     = $_POST['keterangan'];
 
-						$insert = mysqli_query($koneksi, "INSERT INTO datbuadd(namabu, kodebu, dip, diptambah, tgldip, tgltmt, dipgagal, jumlahkartu, keterangandip, namapicbu, telppic, namapetugas, tglambil, namapegawai, status, keterangan)
-															VALUES('$namabu','$kodebu', '$dip', '$diptambah', '$tgldip', '$tgltmt', '$dipgagal', '$jumlahkartu', '$keterangandip', '$namapicbu', '$telppic', '$namapetugas', '$tglambil','$namapegawai','$status','$keterangan')") or die(mysqli_error());
-						if($insert){
-							echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Data Karyawan Berhasil Di Simpan.</div>';
-						}else{
-							echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Ups, Data Karyawan Gagal Di simpan !</div>';
-						}}
+				$insert = mysqli_query($koneksi, "INSERT INTO datbuadd(namabu, kodebu, dip, diptambah, tgldip, tgltmt, dipgagal, jumlahkartu, keterangandip, namapicbu, telppic, namapetugas, tglambil, namapegawai, status, keterangan)
+													VALUES('$namabu','$kodebu', '$dip', '$diptambah', '$tgldip', '$tgltmt', '$dipgagal', '$jumlahkartu', '$keterangandip', '$namapicbu', '$telppic', '$namapetugas', '$tglambil','$namapegawai','$status','$keterangan')") or die(mysqli_error());
+				if($insert){
+					echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Data Karyawan Berhasil Di Simpan.</div>';
+				}else{
+					echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Ups, Data Karyawan Gagal Di simpan !</div>';
+				}
+				}
 			?>
+
 			<div class="row">
+
 				<div class="col-sm-6">
 					<div class="form-box">
 						<form class="form-horizontal" action="" method="post">
@@ -141,6 +147,7 @@ include("koneksi.php");
 						</form>
 					</div>
 				</div>
+
 				<div class="col-sm-6">
 					<div class="form-box">
 						<form class="form-horizontal" action="" method="post">
@@ -196,78 +203,45 @@ include("koneksi.php");
 								<div class="col-sm-6">
 									<input type="submit" name="add" class="btn btn-sm btn-primary" value="Simpan">
 								<a href="index.php" class="btn btn-sm btn-danger">Batal</a>
+								</div>
 							</div>
 						</form>
 					</div>
 				</div>
-					</div>
-				</div>
-			</div>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/bootstrap-datepicker.js"></script>
+
+			</div>	
+		</div>
+	</div>
+
+
 	<script src="js/jquery.js"></script>
 	<script src="js/jquery-ui.min.js"></script>
-	<script src="selectize/js/selectize.min.js."></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script src="selectize/js/standalone/selectize.min.js"></script>
 	<script>
-		 // $('#cari_nmbu').autocomplete({
-		 // 	delay: 0,
-		 // 	source: function( request, response ) {
-			 
-		 // 	  $.ajax({
-		 // 		url: 'carinmbu.php',
-		 // 		dataType: "json",
-		 // 		data: 'cari=' + request.term,
-		 // 		success: function( data ) {
-				  
-		 // 		  response( $.map( data, function( item ) {
-		 // 			return {
-		 // 			  label  : item.nmbu,
-		 // 			  kode   : item.kode					 
-		 // 			}
-		 // 		   }));
-		 // 		},
-		 // 		error: function(e){  
-		 // 		  alert('Error: ' + request);  
-		 // 		}  
-		 // 	   });
-		 // 	},
-		 // 	minLength: 3,
-		 // 	select: function( event, ui ) {
-		 // 	  /* ketika nama yang dicari diklik, maka ditampilkan di form */
-		 // 	  $('#nmbu').val(ui.item.label);
-		 // 	  $('#kode').val(ui.item.kode);
-		 // 	  $('#cari_nmbu').val("");
-		 // 	  return false;
-		 // 	},
-		 // 	focus: function(event, ui) {
-		 // 		return false;
-			// }
-		 // });
-
-
 		$('#cari_nmbu').selectize({
-        valueField: 'kode',
-        labelField: 'nmbu',
-        searchField: 'nmbu',
-        create: false,
+        valueField: 'KdPKS',
+        labelField: 'NmBU',
+        searchField: 'NmBU',
         render: {
             option: function(item, escape) {
                 return '<div style="border-bottom: inset 1px #a5c3df">' +
                         '<span class="title">' +
-                            '<span class="name"><i class="fa fa-user"></i><strong>  Kode : ' + escape(item.username) + '</strong></span>' +
+                            '<span class="name"><i class="fa fa-user"></i>  Kode : ' + escape(item.KdPKS) + '</span>' +
                         '</span><br>' +
-                        '<span class="description"><i class="fa fa-envelope"></i><strong>  Nmbu : ' + escape(item.email) + '</strong></span>' +
+                        '<span class="description"><i class="fa fa-envelope"></i>Nmbu : ' + escape(item.NmBU) + '</span>' +
                         '</div>';
-                    }
+			}
         },
 
         load: function(query, callback) {
             if (!query.length) return callback();
             $.ajax({
-                url: '{{url('admin/api/get-username')}}',
-                type: 'POST',
-                data: {
-                    query: query,
+                url: 'carinmbu.php',
+                type: 'GET',
+                data : {
+                	cari : query
                 },
                 error: function() {
                     callback();
@@ -276,13 +250,14 @@ include("koneksi.php");
                     callback(res);
                 }
             });
-        }
-    });
+	        }
+	    });
 	</script>
+
 	<script>
-	$('.date').datepicker({
-		format: 'yyyy-mm-dd',
-	})
+		$('.date').datepicker({
+			format: 'yyyy-mm-dd',
+		})
 	</script>
 </body>
 </html>
--- carinmbu.php
+++ carinmbu.php
@@ -2,15 +2,17 @@
    include "koneksi.php"; //menyisipkan file koneksi.php
    $cari = isset($_GET['cari']) ? mysqli_real_escape_string($database,$_GET['cari']):'';
    $data = array();
-   $sql  = "SELECT * from refbu where nmbu LIKE '".$cari."%'";
+   $sql  = "SELECT * from refbu where nmbu LIKE '%".$cari. "%' LIMIT 10";
    
    if($res = $database->query($sql)) {
       while ($row = $res->fetch_assoc()) {
 	   $data[] = $row;
       }
    }
+
+   header('Content-Type: application/json');
    echo json_encode($data);
    
    /* tutup koneksinya */
-   $database->close();
+   // $database->close();
 ?>
